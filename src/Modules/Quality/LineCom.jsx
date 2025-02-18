01c7a6d926d2d2b710d2758f29530b3fba2a001f
sakshamdubey11 <saksham.dubey@indi4.io>
2025-02-18 18:08:26 +0530

    Quality api integrated

--- src/Modules/Quality/LineCom.jsx
+++ src/Modules/Quality/LineCom.jsx
@@ -12,27 +12,11 @@ import {
 } from "recharts";
 import Loader from "../../commonComponents/Loader";
 
+const LineCom = ({ data, month }) => {
+  const [isLoading, setIsLoading] = useState(true);
 
-
-const Copq = ({data,month}) => {
   let chartData = [];
-  
-  if (month) {
-    // Filter data for the selected month from day_wise_data
-    chartData = data?.map((D) => ({
-      name: D.Date.slice(-2),
-      target: D.Target,
-      actual: D.Actual,
-    }));
-  } else {
-    chartData = data?.map((D, index) => ({
-      name: D.Month,
-      target: D.Target,
-      actual: D.Actual,
-    }));
-  }
-    const [isLoading, setIsLoading] = useState(true);
-useEffect(() => {
+  useEffect(() => {
     if (data) {
       const timer = setTimeout(() => {
         setIsLoading(false);
@@ -42,17 +26,31 @@ useEffect(() => {
       setIsLoading(true);
     }
   }, [data]);
+
+  if (month) {
+    chartData = data?.map((D) => ({
+      name: D.Date.slice(-2), // Taking last 2 digits of Date for day representation
+      target: D.Target,
+      actual: D.Actual,
+    }));
+  } else {
+    chartData = data?.map((D) => ({
+      name: D.Month,
+      target: D.Target,
+      actual: D.Actual,
+    }));
+  }
+
   return (
-    <Container >
-          {isLoading ? (
-            <div className="d-flex justify-content-center align-items-center mt-4" style={{ height: "300px" }}>
-              <Loader />
-            </div>
-          ) : (
-            <>
-      <Card style={{ border: "none" }}>
+    <Container>
+      {isLoading ? (
+        <div className="d-flex justify-content-center align-items-center mt-4" style={{ height: "250px" }}>
+          <Loader />
+        </div>
+      ) : (
+        <Card style={{ border: "none" }}>
           <ResponsiveContainer width="100%" height={300}>
-            <BarChart
+            <BarChart 
               data={chartData}
               margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
             >
@@ -61,15 +59,14 @@ useEffect(() => {
               <YAxis />
               <Tooltip />
               <Legend />
-              <Bar dataKey="target" fill="#5EDFF8" name="Target" />
+              <Bar dataKey="target" fill="#FB6A4A" name="Target"  />
               <Bar dataKey="actual" fill="#254EDB" name="Actual" />
             </BarChart>
           </ResponsiveContainer>
         </Card>
-      </>
-          )}
+      )}
     </Container>
   );
 };
 
-export default Copq;
+export default LineCom;
--- src/store/quality/qualitySlice.js
+++ src/store/quality/qualitySlice.js
@@ -24,9 +24,9 @@ export const qualityGraphs = createAsyncThunk(
   async ({ type, year, month }) => {
     try {
       const query = month
-        ? `type=${type}&month=${month}&year=${year}`
-        : `type=${type}&year=${year}`;
-      const response = await apiService.get(`dashboard/filter_power/?${query}`);
+        ? `report_type=${type}&month=${month}&year=${year}`
+        : `report_type=${type}&year=${year}`;
+      const response = await apiService.get(`metrics/graph_quality/?${query}`);
       return response.data;
     } catch (error) {
       console.error("Error fetching Graph data:", error);
@@ -54,19 +54,15 @@ export const safetyGraphs = createAsyncThunk(
 const qualitySlice = createSlice({
   name: "quality",
   initialState: {
-    ppm: [],
-    copq: [],
-    sales: [],
-    purchase: [],
-    manpower: [],
-    direct_manpower: [],
-    indirect_manpower: [],
-    process_scrap: [],
-    design_scrap: [],
-    power_cost: [],
-    power_units: [],
-    consumable_costs: [],
-    plan_vs_act: [],
+    qualityData:{
+      CustomerPPM: null,
+      CustomerComplaints: null,
+      Kaizen: null,
+      COPQ: null,
+      SupplierPPM: null,
+      LineGenerationComplaints: null,
+      PlannedRework: null
+    },
     minor: [],
     major: [],
     HRGraphsData:{
@@ -94,49 +90,8 @@ const qualitySlice = createSlice({
       })
       .addCase(qualityGraphs.fulfilled, (state, action) => {
         state.loading = false;
-        switch (action.payload.type) {
-          case "ppm":
-            state.ppm = action.payload;
-            break;
-          case "copq":
-            state.copq = action.payload;
-            break;
-          case "sales":
-            state.sales = action.payload;
-            break;
-          case "purchase":
-            state.purchase = action.payload;
-            break;
-          case "manpower":
-            state.manpower = action.payload;
-            break;
-          case "direct_manpower":
-            state.direct_manpower = action.payload;
-            break;
-          case "indirect_manpower":
-            state.indirect_manpower = action.payload;
-            break;
-          case "process_scrap":
-            state.process_scrap = action.payload;
-            break;
-          case "design_scrap":
-            state.design_scrap = action.payload;
-            break;
-          case "power_cost":
-            state.power_cost = action.payload;
-            break;
-          case "power_units":
-            state.power_units = action.payload;
-            break;
-          case "consumable_costs":
-            state.consumable_costs = action.payload;
-            break;
-          case "plan_vs_act":
-            state.plan_vs_act = action.payload;
-            break;
-          default:
-            state.error = action.error("Getting data of invalid type")
-        }
+        const {type} = action.meta.arg
+        state.qualityData[type] = action.payload
       })
       .addCase(qualityGraphs.rejected, (state, action) => {
         state.loading = false;
